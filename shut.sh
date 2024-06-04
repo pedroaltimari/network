8012bfd9c921e23a763ecc76027f968a0b533bdd
Pedro Altimari <83617644+pedroaltimari@users.noreply.github.com>
2024-06-04 09:57:19 -0300

    script with crontab

--- shut.sh
+++ shut.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/expect
 
-# Defina as variáveis com as informações de login e o IP do switch
 set switch_ip ""
 set username ""
 set password ""
 set interface "GigabitEthernet10"
 
-# Inicie uma sessão telnet com o switch
+# Conexão TELNET
 spawn telnet $switch_ip
 
-# Aguarde o prompt de login e, em seguida, insira seu nome de usuário
+# Insira seu usuário
 expect "Username:"
 send "$username\r"
 
-# Aguarde o prompt de senha e, em seguida, insira sua senha
+# Insira sua senha
 expect "Password:"
 send "$password\r"
 
-# Aguarde o prompt do switch (deve ser adaptado de acordo com o prompt real)
+# Aguardo do prompt do switch
 expect "CBS220>"
 
-# Envie o comando para desligar a interface (substitua o comando conforme necessário)
 send "configure terminal\r"
 expect "CBS220(config)#"
 send "interface $interface\r"
@@ -28,12 +26,10 @@ expect "CBS220(config-if)#"
 send "shutdown\r"
 expect "CBS220(config-if)#"
 
-# Saia da sessão telnet
+# Desconexão TELNET
 send "exit\r"
 expect "CBS220>"
-
-# Feche a sessão telnet
 send "exit\r"
 expect eof
 
-# Eu usei esse script juntamente ao crontab em meu Linux Server
+# Utilizei este script junto ao crontab para fins de automação na minha infra
